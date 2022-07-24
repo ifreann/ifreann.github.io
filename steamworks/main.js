2488a63146c227b9e6f14e1bcdcbea6c6d3e6a7b
Darren <d>
2022-07-24 20:07:06 +0100

    added stupid things

--- steamworks/main.js
+++ steamworks/main.js
@@ -1,12 +1,29 @@
 const minGames = 100;
-const maxGames = 1000000;
-const defaultGames = 10000;
+const maxGames = 1000000000;
+const defaultGames = 1000;
 const loggingThreshold = 10000;
 const loggingEnabled = document.querySelector('#loggingEnabled');
 const output = document.querySelector('div');
 const button = document.querySelector('button');
 const input = document.querySelector('input');
-const label = document.querySelector('label');
+const numberOfGamesLabel = document.querySelector('label[for="numberOfGames"]');
+const humorIdiot = document.querySelector('#humorIdiot');
+const idiotArray = [
+	'[1,2,3]',
+	'[1,3,2]',
+	'[3,1,2]',
+	'[2,1,3]',
+	'[1.2,3]',
+	'[2,3,1]',
+	'[2,1,3]',
+	'[1,3,2]',
+	'[3,1,2]',
+	'[3,2,1]',
+	'[1,2,3]',
+	'[1,2,3]',
+	'[1,2,3]',
+	'[1,3,2]'
+];
 
 input.min = minGames;
 input.max = maxGames;
@@ -27,9 +44,9 @@ input.addEventListener('change', e => {
 		input.value = 100;
 		button.innerText = `Click to play the MHW steamworks ${numberOfGames} times`;
 	}
-	else if (numberOfGames > 1000000) {
-		numberOfGames = 1000000;
-		input.value = 1000000;
+	else if (numberOfGames > maxGames) {
+		numberOfGames = maxGames;
+		input.value = maxGames;
 		button.innerText = `Click to play the MHW steamworks ${Number(numberOfGames).toLocaleString()} times`;
 	}
 });
@@ -38,33 +55,49 @@ button.addEventListener('click', e => {
 	play(+input.value);
 });
 
-label.innerText = `Number of games (${Number(minGames).toLocaleString()} min, ${Number(maxGames).toLocaleString()} max, ${Number(defaultGames).toLocaleString()} default):`;
+numberOfGamesLabel.innerHTML = `Number of games (${Number(minGames).toLocaleString()} min, ${Number(maxGames).toLocaleString()} max <b class="rainbow">(this will crash your browser!!!)</b>, ${Number(defaultGames).toLocaleString()} default):`;
 
 function play(n) {
 
 	// prevent abuse
-	if (n > 1000000) n = 1000000;
-	else if (n < 100) n = 100;
-
-	output.innerText = "";
+	if (n > maxGames) n = maxGames;
+	else if (n < minGames) n = minGames;
 
 	let p1Wins = 0;
 	let p2Wins = 0;
+	const idiot = humorIdiot.checked;
+	let idiotWins = 0;
+	let idiotIndex = 0;
+
+	output.innerText = "";
 
 	while (n) {
 		const correctSequence = JSON.stringify(shuffle([1, 2, 3]));
 		const p1 = '[1,2,3]';
-		const p2 = shuffle([1, 2, 3]);
+		const p2 = JSON.stringify(shuffle([1, 2, 3]));
+		const p3 = idiotArray[idiotIndex] === correctSequence;
 		const p1Won = p1 === correctSequence;
-		const p2Won = JSON.stringify(p2) === correctSequence;
+		const p2Won = p2 === correctSequence;
+		const idiotWon = idiot ? p3 : false;
+
 		if (p1Won) p1Wins++;
 		if (p2Won) p2Wins++;
-		n--;
 
-		if (+input.value <= loggingThreshold && loggingEnabled.checked) output.insertAdjacentHTML('afterbegin', `<p>Correct sequence: ${correctSequence}. Auto-play ${p1Won ? 'won' : 'lost'} with ${p1}, random selection ${p2Won ? 'won' : 'lost'} with [${p2}]</p>`);
+		// if humoring an idiot
+		if (idiot) {
+			if (idiotWon) idiotWins++;
+			idiotIndex++;
+			if (idiotIndex > idiotArray.length - 1) idiotIndex = 0;
+		}
+
+		// if logging is enabled, log
+		if (+input.value <= loggingThreshold && loggingEnabled.checked) output.insertAdjacentHTML('afterbegin', `<p>Correct sequence: ${correctSequence}. Auto-play ${p1Won ? 'won' : 'lost'} with ${p1}, random selection ${p2Won ? 'won' : 'lost'} with [${p2}]${idiot ? `, and some idiot ${idiotWon ? 'won' : 'lost'} with [${p3}].` : ''}</p>`);
+
+		// go next
+		n--;
 	}
 
-	const result = `<p>Over ${Number(numberOfGames).toLocaleString()} games, auto-play won <b>${Number(p1Wins).toLocaleString()}</b> times, and random selection won <b>${Number(p2Wins).toLocaleString()}</b> times.</p><br />`;
+	const result = `<p>Over ${Number(numberOfGames).toLocaleString()} games, auto-play won <b>${Number(p1Wins).toLocaleString()}</b> times, and random selection won <b>${Number(p2Wins).toLocaleString()}</b> times.${idiot ? ` And some idiot won <b>${idiotWins}<b/> times.` : ''}</p><br />`;
 
 	output.innerHTML = result + output.innerHTML;
 
